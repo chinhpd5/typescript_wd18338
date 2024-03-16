4bb143d024f34b8dc8f35eb09541d196d2831847
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-16 18:59:56 +0700

    b6.1 delete

--- wd18338/src/App.tsx
+++ wd18338/src/App.tsx
@@ -2,10 +2,11 @@
 
 import { useEffect, useState } from "react"
 import ProductList from "./components/ProductList";
+import IProduct from "./interfaces/IProduct";
 
 
 function App() {
-  const [list,setList]= useState([]);
+  const [list,setList]= useState<IProduct[]>([]);
 
   useEffect(()=>{
     fetch('http://localhost:3000/product')
@@ -14,14 +15,30 @@ function App() {
       })
       .then(data =>{
         setList(data)
-        
       })
     
   },[])
 
+  function deleteHandle(id: string){
+    fetch(`http://localhost:3000/product/${id}`,{
+      method: "DELETE",
+    })
+    .then((data)=>{
+      return data.json();
+    })
+    .then(data=>{
+      setList(list.filter(item => item.id != data.id));
+    })
+    .catch(()=>{
+      console.log("xóa lỗi");
+      
+    })
+    
+  }
+
   return(
     <>
-      <ProductList listData={list} />
+      <ProductList listData={list} onDelete={deleteHandle} />
     </>
   )
 }
--- wd18338/src/components/ProductList.tsx
+++ wd18338/src/components/ProductList.tsx
@@ -1,19 +1,23 @@
 import IProduct from "../interfaces/IProduct";
 
 type Props ={
-    listData: IProduct[]
+    listData: IProduct[],
+    onDelete: (id: string) => void
 }
 
 function ProductList(props: Props){
 
-    console.log(props);
+    // console.log(props);
     
     return (
         <ul>
             {
                 props.listData.map(item=>{
                     return (
-                        <li key={item.id}>{item.id} | {item.name} | {item.price}</li>
+                        <li key={item.id}>
+                            {item.id} | {item.name} | {item.price}
+                            <button onClick={()=>{props.onDelete(item.id)}}>Xóa</button>
+                        </li>
                     )
                 })
             }
