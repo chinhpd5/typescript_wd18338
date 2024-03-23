13744b2898f60f11b02e72b5e9209f4e7494899e
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-23 19:16:29 +0700

    b9.2

--- wd18338/src/components/Count.tsx
+++ wd18338/src/components/Count.tsx
@@ -1,17 +1,43 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 type PropCount={
 }
 
+function reducer(state:any, action:any){
+    console.log("action: ", action);
+    console.log("state trước: ",state);
+    switch(action){
+        case "ADD":
+            state = state + 1
+            break;
+        case "DELETE":
+            state = state -1;
+            break;
+        case "RESET":
+            state = 0;
+            break;
+        default: 
+            state = state;
+    }
+    console.log("state sau: ",state);
+    
+
+    return state;
+}
+
 function Count(prop: PropCount){
-    const [count, setCount]= useState(0);
+    // const [count, setCount]= useState(0);
+    const [count, dispatch] = useReducer(reducer,0)
 
     return (
         <>
             <h1>Count: {count}</h1>
-            <button className="btn btn-success" onClick={()=>{setCount(count+1)}}>Tăng</button>
-            <button className="btn btn-warning" onClick={()=>{setCount(count-1)}}>Giảm</button>
-            <button className="btn btn-primary" onClick={()=>{setCount(0)}}>Reset</button>
+            <button className="btn btn-success" 
+                onClick={() => {dispatch("ADD")}}>Tăng</button>
+            <button className="btn btn-warning" 
+                onClick={() => {dispatch("DELETE")}}>Giảm</button>
+            <button className="btn btn-primary" 
+                onClick={() => {dispatch("RESET")}}>Reset</button>
         </>
     )
 }
