77c3b82dadebc674e0a4ebb496609c58475f32d5
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-23 19:36:17 +0700

    b9.2

--- wd18338/src/components/Count.tsx
+++ wd18338/src/components/Count.tsx
@@ -20,24 +20,52 @@ function reducer(state:any, action:any){
             state = state;
     }
     console.log("state sau: ",state);
-    
-
     return state;
+}
+function reducer2(state:any, action:any){
+    console.log(action);
+    
+    switch(action.type){
+        case "GAN":
+            return action.payload;
+        default:
+            return state;
+    }
 }
 
 function Count(prop: PropCount){
     // const [count, setCount]= useState(0);
     const [count, dispatch] = useReducer(reducer,0)
+    const [count2,dispatch2] = useReducer(reducer2,0);
 
     return (
         <>
-            <h1>Count: {count}</h1>
-            <button className="btn btn-success" 
-                onClick={() => {dispatch("ADD")}}>Tăng</button>
-            <button className="btn btn-warning" 
-                onClick={() => {dispatch("DELETE")}}>Giảm</button>
-            <button className="btn btn-primary" 
-                onClick={() => {dispatch("RESET")}}>Reset</button>
+            <div>
+                <h1>Count: {count}</h1>
+                <button className="btn btn-success" 
+                    onClick={() => {dispatch("ADD")}}>
+                        Tăng
+                </button>
+                <button className="btn btn-warning" 
+                    onClick={() => {dispatch("DELETE")}}>
+                        Giảm
+                </button>
+                <button className="btn btn-primary" 
+                    onClick={() => {dispatch("RESET")}}>Reset</button>
+            </div>
+
+            <div>
+                <h1>Count '2': {count2}</h1>
+                <button className="btn btn-info" onClick={()=>{
+                    dispatch2({
+                        type: "GAN",
+                        payload: 10
+                    })
+                }
+                }>
+                        Gán gt
+                </button>
+            </div>
         </>
     )
 }
